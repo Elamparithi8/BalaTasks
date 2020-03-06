be9ee624595c5ce55304b6fef410f54c04a8d05f
Elamparithi8 <61818337+Elamparithi8@users.noreply.github.com>
2020-03-06 18:24:38 +0530

    Update Chess.java

--- Chess.java
+++ Chess.java
@@ -1,3 +1,29 @@
+//code updated
+public class Chess{
+        public static void main(String[] args) {
+            int chess = 1;
+            int chess1 = 0;
+            for (int i = 0; i < 4; i++) {
+                for (int k = 0; k < 4; k++) {
+                    if(i%2==0 && k%2==0){
+                        System.out.print(chess);
+                    }
+                    else if(i%2==0 && k%2!=0){
+                        System.out.print(chess1);
+                    }
+                    else if(i%2!=0 && k%2==0){
+                        System.out.print(chess1);
+                    }
+                    else{
+                        System.out.print(chess);
+
+                    }
+                }
+                System.out.println();
+            }
+        }
+}
+/*
 public class Chess{
         public static void main(String[] args) {
             int chess = 1;
@@ -27,5 +53,6 @@ public class Chess{
         }
 
 }
+
 
 
