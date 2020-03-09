52abf89f548c51ca462d77d2756c8ceb726fc319
Elamparithi8 <61818337+Elamparithi8@users.noreply.github.com>
2020-03-09 17:42:41 +0530

    Update Chess.java

--- Chess.java
+++ Chess.java
@@ -1,29 +1,3 @@
-//code updated
-public class Chess{
-        public static void main(String[] args) {
-            int chess = 1;
-            int chess1 = 0;
-            for (int i = 0; i < 4; i++) {
-                for (int k = 0; k < 4; k++) {
-                    if(i%2==0 && k%2==0){
-                        System.out.print(chess);
-                    }
-                    else if(i%2==0 && k%2!=0){
-                        System.out.print(chess1);
-                    }
-                    else if(i%2!=0 && k%2==0){
-                        System.out.print(chess1);
-                    }
-                    else{
-                        System.out.print(chess);
-
-                    }
-                }
-                System.out.println();
-            }
-        }
-}
-/*
 public class Chess{
         public static void main(String[] args) {
             int chess = 1;
@@ -52,6 +26,39 @@ public class Chess{
             }
         }
 
+}
+//revised code after the session
+
+public class Chess{
+        public static void main(String[] args) {
+            int chess_black = 1;
+            int chess_white = 0;
+            int rows_column = 4;    
+            for (int i = 0; i < rows_column; i++)
+            {
+                for (int k = 0; k < rows_column; k++)
+                {
+                    if(i%2==0 && k%2==0)
+                    {
+                        System.out.print(chess_black);
+                    }
+                    else if(i%2==0 && k%2!=0)
+                    {
+                        System.out.print(chess_white);
+                    }
+                    else if(i%2!=0 && k%2==0)
+                    {
+                        System.out.print(chess_white);
+                    }
+                    else
+                    {
+                        System.out.print(chess_black);
+
+                    }
+                }
+                System.out.println();
+            }
+        }
 }
 
 
