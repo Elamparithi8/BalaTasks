dee0e8281dcd7799165102ef413e8ffb01eb0ce1
Elamparithi8 <61818337+Elamparithi8@users.noreply.github.com>
2020-03-09 19:04:13 +0530

    Update AddEven.java

--- AddEven.java
+++ AddEven.java
@@ -1,6 +1,8 @@
-public class AddEven{
+public class AddEven
+{
 
-        public static void main(String[] args) {
+        public static void main(String[] args) 
+        {
 
            int input = 2345;
 
