de69013b92e65dbd147a07d7c387fb885adacfe8
Elamparithi8 <61818337+Elamparithi8@users.noreply.github.com>
2020-03-09 17:15:23 +0530

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -13,3 +13,22 @@ public class Triangle{
         }
 
 }
+//revised code after the session
+public class Triangle{
+    public static void main(String[] args) {
+        int intial_number = 1;
+        int rows=4;
+        int intial_columns = rows;
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<intial_columns;j++)
+            {
+                System.out.print(num+"\t");
+                num++;
+            }
+            System.out.println();
+            intial_columns--;
+        }
+    }
+
+}
