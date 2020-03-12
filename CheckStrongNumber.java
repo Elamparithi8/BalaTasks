c88b09353fb735948d2c88d7b76eff92a5da030b
Elamparithi8 <61818337+Elamparithi8@users.noreply.github.com>
2020-03-12 16:17:38 +0530

    Create CheckStrongNumber.java

---
+++ CheckStrongNumber.java
@@ -0,0 +1,70 @@
+public class public class CheckStrongNumber{
+    public static int factorial(int num){
+        int answer = 1;
+        while(num>0)
+        {
+            answer *= num;
+            num--;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+
+       int input = 145;
+       int copy_input = input;
+       int output = 0;
+       while(copy_input>0)
+       {
+            int rem = copy_input%10;
+            output+=factorial(rem);
+            copy_input = (int)copy_input/10;
+
+       }
+       if (input==output)
+       {
+           System.out.println("Strong number");
+       }
+       else
+       {
+           System.out.println("Not a Strong Number");
+       }
+
+       }
+
+}
+
+{
+    public static int factorial(int num){
+        int answer = 1;
+        while(num>0)
+        {
+            answer *= num;
+            num--;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+
+       int input = 145;
+       int copy_input = input;
+       int output = 0;
+       while(copy_input>0)
+       {
+            int rem = copy_input%10;
+            output+=factorial(rem);
+            copy_input = (int)copy_input/10;
+
+       }
+       if (input==output)
+       {
+           System.out.println("Strong number");
+       }
+       else
+       {
+           System.out.println("Not a Strong Number");
+       }
+
+       }
+
+}
+
