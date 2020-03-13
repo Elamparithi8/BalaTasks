a5d8208a152471047ca602d75ba6f29508382703
Elamparithi8 <61818337+Elamparithi8@users.noreply.github.com>
2020-03-13 11:39:56 +0530

    Update RemoveString.java

--- RemoveString.java
+++ RemoveString.java
@@ -1,8 +1,25 @@
+import java.util.Arrays;
+
 public class RemoveString{
-    public static void main(String[] args) {
-        String sentance = "I am elamparithi";
-        String remove = "am";
-        sentance=sentance.replace(remove,"");
-        System.out.println(sentance);
+        public static void main(String[] args) {
+            String sentance = "I am elamparithi am running";
+            String remove = "am";
+            int remove_len = remove.length()-1;
+            String output = "";
+            if(sentance.contains(remove)){
+                String[] words = sentance.split(" ");
+                for (String k:words){
+                    if(k.equals(remove)){
+
+                    }
+                    else{
+                        output+=k+" ";
+                    }
+                }
+            }
+            else{
+                System.out.println(sentance);
+            }
+            System.out.println(output);
+        }
     }
-}
