52ab83d95de0504520992971b6c52f2a93f1ae9a
Elamparithi8 <61818337+Elamparithi8@users.noreply.github.com>
2020-03-13 10:48:45 +0530

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,37 +1,31 @@
 import java.util.Arrays;
 public class SelectionSort{
     public static void main(String[] args) {
-        int[] input = {18 ,9 ,33 ,4 ,84 ,32 };
-        int[] copy_input = input;
+        int[] input = {18 ,9 ,33 ,4 ,84 ,32,32,42,11,44,21,52 };
+        int index = 0;
         int minimum = 0;
-        int[] output = new int[copy_input.length];
+        int[] output = new int[input.length];
 
-        for (int i=0;i<copy_input.length;i++)
+        for (int i=0;i<input.length;i++)
         {
-            minimum = copy_input[i];
-            for (int j=0;j<copy_input.length;j++)
+            index = i;
+            minimum = input[i];
+            for (int j=i;j<input.length;j++)
             {
-                 if(minimum>copy_input[j])
-                 {
-                     minimum=copy_input[j];
-                 }
-                 else
-                 {
-                     minimum=minimum;
-                 }
-            }
-            output[i]=minimum;
-            for(int k=0;k<copy_input.length;k++)
-            {
-                if(copy_input[k]==minimum){
-                    copy_input[k]=1000;
+                if(minimum>input[j])
+                {
+                    minimum=input[j];
+                    index = j;
                 }
                 else
                 {
-                    copy_input[k]=copy_input[k];
+                    minimum=minimum;
                 }
             }
+            input[index]=input[i];
+            input[i]=minimum;
+
         }
-        System.out.println(Arrays.toString(output));
+        System.out.println(Arrays.toString(input));
     }
 }
