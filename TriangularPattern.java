06fd25c661c31131cb2ef3fd7973e3b265385216
Elamparithi8 <61818337+Elamparithi8@users.noreply.github.com>
2020-03-17 12:40:56 +0530

    Update TriangularPattern.java

--- TriangularPattern.java
+++ TriangularPattern.java
@@ -1,32 +1,29 @@
 public class TriangularPattern{
     public static void main(String[] args) {
 
-       int input = 7;
-
-       int column = 1;
-
-       int initial_num = 1;
-       
-       for(int i=0;i<input;i++)
-       {
-            for(int j=0;j<column;j++)
+        int input = 7;
+        int column = 1;
+        int triangular_array [][] = new int[input][];
+        for(int i=0;i<triangular_array.length;i++)
+        {
+            int initial_num = 0;
+            triangular_array[i] = new int[column];
+            int j;
+            for(j=0;j<=(triangular_array[i].length-(triangular_array[i].length/2)-1);j++)
             {
+                initial_num+=1;
                 System.out.print(initial_num+" ");
-                if(j<Math.round(column/2))
-                {
-                    initial_num++;
-                }
-                else
-                {
-                       initial_num--;
-                }
-
+            }
+            for(int k=j;k<triangular_array[i].length;k++)
+            {
+                initial_num--;
+                System.out.print(initial_num+" ");
             }
             column+=2;
-            initial_num=1;
-           System.out.println();
 
-       }
+            System.out.println();
+
+        }
 
     }
 }
