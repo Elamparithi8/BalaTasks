06bbda6cd37577e3603ee8a7855249f53eb6e500
Elamparithi8 <61818337+Elamparithi8@users.noreply.github.com>
2020-03-18 10:16:49 +0530

    Update RemoveString.java

--- RemoveString.java
+++ RemoveString.java
@@ -2,24 +2,22 @@ import java.util.Arrays;
 
 public class RemoveString{
         public static void main(String[] args) {
-            String sentance = "I am elamparithi am running";
-            String remove = "am";
-            int remove_len = remove.length()-1;
-            String output = "";
-            if(sentance.contains(remove)){
-                String[] words = sentance.split(" ");
-                for (String k:words){
-                    if(k.equals(remove)){
-
-                    }
-                    else{
-                        output+=k+" ";
-                    }
-                }
+        String sentance = "I am elamparithi am running";
+        String remove = "am";
+        int remove_len = remove.length()-1;
+        String output = "";
+        if(sentance.contains(remove))
+        {
+            String[] words = sentance.split(remove);
+            for (String k:words)
+            {
+                    output+=k+"";
             }
-            else{
-                System.out.println(sentance);
-            }
-            System.out.println(output);
         }
+        else
+        {
+            System.out.println(sentance);
+        }
+        System.out.println(output);
     }
+ }
