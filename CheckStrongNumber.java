e46941eebcc234907e50d731304bc883f04261bb
Elamparithi8 <61818337+Elamparithi8@users.noreply.github.com>
2020-03-18 10:03:24 +0530

    Update CheckStrongNumber.java

--- CheckStrongNumber.java
+++ CheckStrongNumber.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class CheckStrongNumber{
     public static int factorial(int num){
         int answer = 1;
@@ -10,10 +11,13 @@ public class CheckStrongNumber{
     }
     public static void main(String[] args) {
 
-       int input = 145;
+       Scanner scanner = new Scanner(System.in);
+       System.out.print("Enter the number :");
+       int input = (int)scanner.nextInt();
        int copy_input = input;
        int output = 0;
-       while(copy_input>0)
+       
+        while(copy_input>0)
        {
             int rem = copy_input%10;
             output+=factorial(rem);
