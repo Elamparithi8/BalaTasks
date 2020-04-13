372c3314531e7c802f22bc79b70990640d3838ea
Elamparithi8 <61818337+Elamparithi8@users.noreply.github.com>
2020-04-13 14:50:57 +0530

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -246,7 +246,7 @@ public class Minesweeper
             }
         }
     }
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
         Minesweeper mines = new Minesweeper();
         mines.startGame();
     }
