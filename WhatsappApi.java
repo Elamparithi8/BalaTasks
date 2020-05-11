2a2af43d8e2712b1535416a94e8a11ca0e5a6e0c
Elamparithi8 <61818337+Elamparithi8@users.noreply.github.com>
2020-05-11 15:17:57 +0530

    Update WhatsappApi.java

--- WhatsappApi.java
+++ WhatsappApi.java
@@ -1,18 +1,112 @@
 import java.util.*;
+class Contacts
+{
+    static HashMap<Integer,Contacts> contacts_array = new HashMap<>();
+    static HashMap<Integer,Integer> chats_array = new HashMap<>();
+    static HashMap<Integer,LinkedList> mssgs_array = new HashMap<>();
+    String name;
+    String number;
+    static int contact_id = 100;
+    public Contacts()
+    {
+      contact_id++;
+    }
+    public void setName(String nm)
+    {
+        this.name = nm;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setNumber(String num)
+    {
+        this.number = num;
+    }
+    public String getNumber()
+    {
+        return number;
+    }
+    public int getContact_id()
+    {
+        return contact_id;
+    }
 
-public class WhatsappApi{
-    static HashMap<String,String> contacts_array = new HashMap<>();
-    static HashMap<String, LinkedList> mssgs_array = new HashMap<>();
+    public static void main(String[] args) {
+
+    }
+}
+class Chats{
+    static int chat_id = 0;
+    public Chats()
+    {
+        chat_id++;
+    }
+    public int getChat_id()
+    {
+        return chat_id;
+    }
+    public int getContact_id(Contacts c)
+    {
+        return c.getContact_id();
+    }
+}
+class Messages{
+    static int mssg_id = 0;
+    static String message;
+    static LinkedList<String> mssgs_all =new LinkedList<>();
+    ;
+    public Messages()
+    {
+        mssg_id++;
+
+    }
+    public int getMssg_id()
+    {
+        return mssg_id;
+    }
+    public int getChat_id(Chats ch1)
+    {
+
+        return ch1.getChat_id();
+    }
+    public void setMessage(String mssg)
+    {
+        this.message = mssg;
+    }
+    public String getMessage()
+    {
+        return message;
+    }
+    public int getContact_id(Contacts c)
+    {
+        return c.getContact_id();
+    }
+}
+public class WhatsappApi extends Contacts{
     public static void showContacts()
     {
         Set list_of_contact = contacts_array.keySet();
         Iterator iterator = list_of_contact.iterator();
         while (iterator.hasNext()) {
-            String num = (String) iterator.next();
-            System.out.println("Name :  "+contacts_array.get(num));
+            int num = (int)iterator.next();
+            Contacts c = contacts_array.get(num);
+            System.out.println(c.getName());
         }
     }
-    public static LinkedList showMessages(String number)
+    public static void showChats()
+    {
+        Set list_of_contact = chats_array.keySet();
+        Iterator iterator = list_of_contact.iterator();
+        while (iterator.hasNext()) {
+            int num = (int)iterator.next();
+            int cid = chats_array.get(num);
+            Contacts c = contacts_array.get(cid);
+            System.out.println(c.getName());
+
+        }
+    }
+    public static LinkedList showMessages(int number)
     {
         LinkedList list = mssgs_array.get(number);
         Iterator iterator = list.iterator();
@@ -29,19 +123,29 @@ public class WhatsappApi{
         String num = scanner.next();
         System.out.println("Enter the name");
         String name = scanner.next();
-        if(!contacts_array.containsKey(num)) {
-            contacts_array.put(num, name);
-            System.out.println("create successfully");
-        }
-        else
-        {
-            System.out.println("this contacts is already exist");
-        }
+        Contacts contacts = new Contacts();
+        contacts.setNumber(num);
+        contacts.setName(name);
+        contacts_array.put(contacts.getContact_id(),contacts);
+        System.out.println("create successfully");
+
+
     }
-    @SuppressWarnings("unchecked")
-    public static void sendMessages(LinkedList incoming,String num)
+    public static void createChats(int num)
     {
-        incoming = new LinkedList<>();
+        Chats chats = new Chats();
+        chats_array.put(chats.getChat_id(),num);
+        System.out.println("chat create successfully");
+        Messages messages = new Messages();
+        LinkedList linkedList = new LinkedList();
+        mssgs_array.put(chats.getChat_id(),linkedList);
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void sendMessages(int num)
+    {
+        LinkedList incoming = new LinkedList<>();
         try {
             incoming = showMessages(num);
         }
@@ -51,144 +155,158 @@ public class WhatsappApi{
         }
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the mssg do you want to sent");
-        String mssg = ">"+scanner.nextLine();
-        incoming.add(mssg);
+        Messages messages = new Messages();
+        messages.setMessage(scanner.nextLine());
+        incoming.add(messages.getMessage());
         mssgs_array.put(num, incoming);
         System.out.println("mssg sended");
     }
-    public static void editContacts(String num)
+    public static void editContacts(int num)
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Number : "+num);
-        System.out.println("Name : "+contacts_array.get(num));
+        showContacts();
         System.out.println("Enter the name to change");
         String name = scanner.nextLine();
         System.out.println("Enter the mobile number to change else enter the same");
         String number = scanner.next();
-        contacts_array.remove(num);
-        contacts_array.put(number, name);
+        Contacts contacts = contacts_array.get(num);
+        contacts.setNumber(number);
+        contacts.setName(name);
+        System.out.println(contacts.getContact_id());
+        contacts_array.put(contacts.getContact_id(),contacts);
+        System.out.println("edited successfully");
     }
-    public static void blockContact(String num)
+    public static void blockContact(int num)
     {
         contacts_array.remove(num);
         System.out.println("deleted successfully");
     }
     @SuppressWarnings("unchecked")
-    public static void clearChat(String num,LinkedList incoming)
+    public static void clearChat(int num)
     {
-        incoming = new LinkedList<>();
+        LinkedList incoming = new LinkedList<>();
         mssgs_array.remove(num);
         mssgs_array.put(num, incoming);
         System.out.println("chat cleared successfully");
     }
-    public static void start()
+    public static void checkingURLs()
     {
-        LinkedList<String> incoming = new LinkedList<>();
-        contacts_array.put("9360619395","Elam");
-        contacts_array.put("9876543210","Gopi");
-        incoming.add(">how are you?");
-        incoming.add("<fine!");
-        incoming.add(">what are you doing?");
-        incoming.add(">answer me");
-        incoming.add("<playing snake and ladder");
-        mssgs_array.put("9876543210",incoming);
-        incoming = new LinkedList<>();
-        incoming.add(">good morining");
-        incoming.add("<gm");
-        mssgs_array.put("9360619395",incoming);
-        boolean begin = true;
-        while(begin!=false) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter the method and url");
-            String input = scanner.nextLine();
-            String[] words = input.split(" ");
-            String method = words[0];
-            String url = words[1];
-            String[] split_url = (url.split("/"));
-            int len = split_url.length;
-            if (method.equals("GET")) {
-                for (int i = 1; i < len; i++) {
-                    int j = i;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the method and url");
+        String input = scanner.nextLine();
+        String[] words = input.split(" ");
+        String method = words[0];
+        String url = words[1];
+        String[] split_url = (url.split("/"));
+        int len = split_url.length;
+        if (method.equals("GET")) {
+            for (int i = 1; i < len; i++) {
+                int j = i;
+                j++;
+                if (split_url[i].equals("contacts") && i == len - 1) {
+                    //show list of contacts
+                    showContacts();
+                }
+                else if (split_url[i].equals("chats") && i == len - 1) {
+                    //show list of chats
+                    showChats();
+                }
+                else if (split_url[i].equals("chats") && mssgs_array.containsKey(Integer.parseInt(split_url[j])) && j == len - 1) {
+                    showMessages(Integer.parseInt(split_url[j]));
+                } else if (split_url[i].equals("contacts") && contacts_array.containsKey(Integer.parseInt(split_url[j]))) {
                     j++;
-                    if (split_url[i].equals("contacts") && i == len - 1) {
-                        //show list of contacts
-                        showContacts();
-                    } else if (split_url[i].equals("contacts") && mssgs_array.containsKey(split_url[j]) && j == len - 1) {
-                        showMessages(split_url[j]);
-                    } else if (split_url[i].equals("contacts") && contacts_array.containsKey(split_url[j])) {
-                        j++;
-                        if (j == len - 1 && split_url[j].equals("about")) {
-                            j--;
-                            System.out.println("Number :  "+split_url[j]);
-                            System.out.println("Name :  "+contacts_array.get(split_url[j]));
-                        }
-
+                    if (j == len - 1 && split_url[j].equals("about")) {
+                        j--;
+                        System.out.println("Number :  "+Integer.parseInt(split_url[j]));
+                        System.out.println("Name :  "+contacts_array.get(Integer.parseInt(split_url[j])));
                     }
-                    else
-                    {
-                        System.out.println("enter the wrong URL ");
-                    }
-                    break;
 
                 }
-            } else if (method.equals("POST")) {
-                for (int i = 1; i < len; i++) {
-                    int j = i;
-                    j++;
-                    if (split_url[i].equals("contacts") && i == len - 1) {
-                        createContacts();
-                    } else if (split_url[i].equals("contacts") && contacts_array.containsKey(split_url[j]) && j == len - 1) {
-                        sendMessages(incoming, split_url[j]);
-                    }
-                    else
-                    {
-                        System.out.println("enter the wrong URL ");
-                    }
-                    break;
+                else
+                {
+                    System.out.println("enter the wrong URL ");
+                }
+                break;
 
-                }
-            } else if (method.equals("PUT")) {
-                for (int i = 1; i < len; i++) {
-                    int j = i;
-                    j++;
-                    if (split_url[i].equals("contacts") && contacts_array.containsKey(split_url[j]) && j == len - 1) {
-                        editContacts(split_url[j]);
-                    }
-                    else
-                    {
-                        System.out.println("enter the wrong URL ");
-                    }
-                    break;
-                }
-            } else if (method.equals("DELETE")) {
-                for (int i = 1; i < len; i++) {
-                    int j = i;
-                    j++;
-                    if (split_url[i].equals("contacts") && contacts_array.containsKey(split_url[j]) && j == len - 1) {
-                        blockContact(split_url[j]);
-                    } else if (split_url[i].equals("contacts") && contacts_array.containsKey(split_url[j])) {
-                        j++;
-                        if (j == len - 1 && split_url[j].equals("clear")) {
-                            j--;
-                            clearChat(split_url[j], incoming);
-                        }
-
-                    }
-                    else
-                    {
-                        System.out.println("enter the wrong URL ");
-                    }
-                    break;
-                }
             }
-            else{
-                System.out.println("Wrong method");
+        } else if (method.equals("POST")) {
+            for (int i = 1; i < len; i++) {
+                int j = i;
+                j++;
+                if (split_url[i].equals("contacts") && i == len - 1) {
+                    createContacts();
+                }
+                else if(split_url[i].equals("chats") && contacts_array.containsKey(Integer.parseInt(split_url[j])) && j == len - 1)
+                {
+                    createChats(Integer.parseInt(split_url[j]));
+                }
+                else if (split_url[i].equals("chats") && mssgs_array.containsKey(Integer.parseInt(split_url[j])) && j == len - 1) {
+                    sendMessages(Integer.parseInt(split_url[j]));
+                }
+                else
+                {
+                    System.out.println("enter the wrong URL ");
+                }
+                break;
+
             }
-            System.out.println("Do you want to enter URL true or false");
-            begin = scanner.nextBoolean();
+        } else if (method.equals("PUT")) {
+            for (int i = 1; i < len; i++) {
+                int j = i;
+                j++;
+                if (split_url[i].equals("contacts") && contacts_array.containsKey(Integer.parseInt(split_url[j])) && j == len - 1) {
+                    editContacts(Integer.parseInt(split_url[j]));
+                }
+                else
+                {
+                    System.out.println("enter the wrong URL ");
+                }
+                break;
+            }
+        }else if (method.equals("DELETE")) {
+            for (int i = 1; i < len; i++) {
+                int j = i;
+                j++;
+                if (split_url[i].equals("contacts") && contacts_array.containsKey(Integer.parseInt(split_url[j])) && j == len - 1) {
+                    blockContact(Integer.parseInt(split_url[j]));
+                } else if (split_url[i].equals("chats") && mssgs_array.containsKey(Integer.parseInt(split_url[j]))){
+                    j++;
+                    if (j == len - 1 && split_url[j].equals("clear")) {
+                        j--;
+                        clearChat(Integer.parseInt(split_url[j]));
+                    }
+
+                }
+                else
+                {
+                    System.out.println("enter the wrong URL ");
+                }
+                break;
+            }
+        }
+        else{
+            System.out.println("Wrong method");
         }
     }
     public static void main(String[] args) {
-        start();
+        Contacts c1 = new Contacts();
+        c1.setNumber("9962072701");
+        c1.setName("Sathyaraj");
+        contacts_array.put(c1.getContact_id(),c1);
+        Chats ch1 = new Chats();
+        chats_array.put(ch1.getChat_id(),ch1.getContact_id(c1));
+        Messages mssggg = new Messages();
+        mssggg.setMessage("hii sathyaraj");
+        LinkedList<String> mssg_all = new LinkedList<>();
+        mssg_all.add(mssggg.getMessage());
+        mssgs_array.put(ch1.getChat_id(),mssg_all);
+
+        boolean begin = true;
+        while(begin!=false) {
+            checkingURLs();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Do you want to enter URL true or false");
+            begin = scanner.nextBoolean();
+        }
     }
 }
